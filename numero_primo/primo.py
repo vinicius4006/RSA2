5e7e3f987465de7dc9bf4653be34fb94d7e93342
Vinicius S R França <vinicius@pop-os.localdomain>
2023-05-15 22:40:37 -0300

    otimizacao numero primo

--- numero_primo/primo.py
+++ numero_primo/primo.py
@@ -1,60 +1,20 @@
+import math
+
 def is_prime(num):
-    result = False
     if num <= 1:
-        return result
-    if num <= 3 or num == 5 or num == 7:
-        result = True
-        return result
-    
-    if is_two(num):
-        return result
-    
-    if is_three(num):
-        return result
-    
-    if is_five(num):
-        return result
-    
-    if is_seven(num):
-        return result
-    
-    denominator = 11
-    quotient = num // denominator
-    stop = False
-    
-    while True:
-        if stop:
-            break
-        if quotient < denominator:
-            stop = True
-            result = True
-        if num % denominator == 0:
-            stop = True
-        else:
-            denominator += 1
-    
-    return result
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
 
+    # Verificar divisibilidade apenas até a raiz quadrada do número
+    sqrt_num = int(math.sqrt(num)) + 1
+    for divisor in range(5, sqrt_num, 6):
+        if num % divisor == 0 or num % (divisor + 2) == 0:
+            return False
 
-def is_two(num):
-    return num % 2 == 0
+    return True
 
-
-def is_three(num):
-    s = list(str(num))
-    soma = sum(int(digit) for digit in s)
-    return soma % 3 == 0
-
-
-def is_five(num):
-    s = list(str(num))
-    return s[-1] == "5" or s[-1] == "0"
-
-
-def is_seven(num):
-    return num % 7 == 0
-
-
-a = is_prime(14615016373309029182036848327162830196559325429763531029354160528133907320308105037998849027227)
-
+a = is_prime(98764321261)
 print(a)

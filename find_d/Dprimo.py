d7e09e20c59c1a2770355d02ae2d093d7fce870d
benborges <borges.benjamim@gmail.com>
2023-05-19 00:15:27 -0300

    atualização Dprimo

--- find_d/Dprimo.py
+++ find_d/Dprimo.py
@@ -7,20 +7,25 @@ def mdc(n1: int, n2: int) -> int:
         n1, n2 = n2, n1 % n2
     return n1
 
+def e_primo(num: int) -> bool:
+    """Verifica se um número é primo"""
+    if num < 2:
+        return False
+    for i in range(2, isqrt(num) + 1):
+        if num % i == 0:
+            return False
+    return True
 
 def co_primos(p: int, q: int, z: int) -> int:
     """Devolve um número primo aleatório entre 1 e z que seja coprimo com z"""
     b = 1
     coprimos = []
     while len(coprimos) < z:
-        res = mdc(z, b)
-        if res == 1:
+        if mdc(b, z) == 1:
             coprimos.append(b)
         b += 1
 
-    primos = [num for num in coprimos if all(num % i != 0 for i in range(2, isqrt(num) + 1))]
-    """verifica se o número fornecido pela lista, é primo"""
-
+    primos = [num for num in coprimos if e_primo(num)]
     d = sample(primos, 1)
 
     return int(d[0])

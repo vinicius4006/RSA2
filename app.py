7cbbe5e360a39cb0472e9cfd03845ab7c3311346
Elvis-Almeida <lindete.r.almeida@gmail.com>
2023-05-30 23:32:02 -0300

    Atualizando interface

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from RSA import generate_keys, encrypt, decrypt
 import json
 import sys
 from PyQt5.QtWidgets import *
+from time import sleep
+
+#  ---------------  Função do servidor  --------------------
 
 def chat_receive(port, private_key):
     try:
@@ -19,13 +22,19 @@ def chat_receive(port, private_key):
                 v = 1
                 message = data.decode('utf-8')
                 public_key2 = json.loads(message)
+
                 with open('public_key.json', 'w') as file:
                     json.dump(public_key2, file)
+
                 message_display.append('Chave recebida')
                 
             else:
                 message = decrypt(data, private_key)
                 message_display.append(message)
+            
+            sleep(0.01)
+            scrollbar = message_display.verticalScrollBar()
+            scrollbar.setValue(scrollbar.maximum())
             
     except socket.error as e:
         print("Erro ao receber mensagem:", str(e))
@@ -33,28 +42,32 @@ def chat_receive(port, private_key):
     finally:
         sock.close()
 
+#  ---------------  Função do cliente  --------------------
 
 def chat_send(public_key, host, port, seu_nome, lock, condition):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        label_status.setText('Aperte Enviar para enviar a chave publica: ')
-
-        with lock:
-            condition.wait() 
-
         sock.sendto(str(public_key).encode('UTF-8'), (host, port))
 
         while True:
             message = receber_texto('Digite a mensagem: ' , lock, condition)
             message2 = 'Você: ' + message
             message = seu_nome + ': ' + message
-            message_display.append(message2)
             
-            with open('public_key.json', 'r') as file:
-                public_key2 = json.load(file)
+            try:
+                with open('public_key.json', 'r') as file:
+                    public_key2 = json.load(file)
 
-            message = encrypt(message, public_key2)
-            sock.sendto(message, (host, port))
+                message = encrypt(message, public_key2)
+                sock.sendto(message, (host, port))
+                message_display.append(message2)
+
+            except:
+                message_display.append("Você não possui a chave!")
+
+            sleep(0.01)
+            scrollbar = message_display.verticalScrollBar()
+            scrollbar.setValue(scrollbar.maximum())
 
     except socket.error as e:
         print("Erro ao enviar mensagem:", str(e))
@@ -65,18 +78,29 @@ def chat_send(public_key, host, port, seu_nome, lock, condition):
 #  ---------------  Inicia as threads de servidor e cliente  --------------------
 
 def client_server():
-    public_key, private_key = generate_keys(100, 200)
+
+    try:
+        with open('mykeys.json', 'r') as file:
+            mykeys = list(json.load(file))
+
+        public_key = mykeys[0]
+        private_key = mykeys[1]
+
+    except:
+        label_status.setText('Gerando Chaves')
+        public_key, private_key = generate_keys(2000, 5000)
+        with open('mykeys.json', 'w') as file:
+            json.dump([public_key, private_key], file)
 
     tr1 = Thread(target=chat_receive, args=(12345, private_key))
     tr1.daemon = True
+    tr1.start()
 
     seu_nome = receber_texto('Digite seu nome: ' , lock, condition)
     host = receber_texto('Digite o IP de destino: ' , lock, condition)
 
     tr2 = Thread(target=chat_send, args=(public_key ,host, 12345, seu_nome, lock, condition))
     tr2.daemon = True
-
-    tr1.start()
     tr2.start()
     
 #  ---------------  Destrava as treads em aguardo  --------------------
@@ -98,19 +122,19 @@ def receber_texto(texto , lock, condition):
 
 #  ---------------  Inicia a thread client_server  --------------------
 
-def remover_widget():
-    button = app.focusWidget()
-    if button:
-        layout.removeWidget(button)
-        button.deleteLater()
+def remover_widget(send_button2):
+
+    layout.removeWidget(send_button2)
+    send_button2.deleteLater()
 
     send_button = QPushButton("Enviar")
     send_button.clicked.connect(lambda: destravar_tread(condition))
     layout.addWidget(send_button)
 
+    message_input.returnPressed.connect(lambda: destravar_tread(condition)) 
+
     tr3 = Thread(target=client_server) 
     tr3.start()
-
 
 #  ---------------  Bloqueadores de threads  --------------------
 
@@ -137,8 +161,10 @@ message_input = QLineEdit()
 layout.addWidget(message_input)
 
 send_button = QPushButton("Iniciar")
-send_button.clicked.connect(remover_widget)
+send_button.clicked.connect(lambda: remover_widget(send_button))
 layout.addWidget(send_button)
+
+send_button.click()
 
 window.show()
 app.exec_()
